1303d1c6a913b332a650c93668e1311a870d1c7c
Iracy Maiany <iracymaiany@hotmail.com>
2023-05-09 12:48:44 -0300

    Edition text for download and integration

--- Databases download and integration.R
+++ Databases download and integration.R
@@ -1,3 +1,6 @@
+## %######################################################%##
+# Written by: Santiago J.E. Velazco, M. Brooke Rose, & Janet Franklin
+
 # pacots para usar
 devtools::install_github("liibre/Rocc")
 library(Rocc)
@@ -16,23 +19,242 @@ require(ggplot2) # Plot data
 require(sf) # For handling spatial data
 require(maps) # A spatial database of country boundaries
 
+## %######################################################%##
+#                                                          #
+####                    Species list                    ####
+#                                                          #
+## %######################################################%##
+
+spp <- c("Peltophorum dubium", "Ceiba chodatii", "Asdf sfd")
+
+getwd()
+dirs <- file.path(getwd(), "raw_data")
+dirs
+dir.create(dirs) # Function for creating the directory where each dataset will be saved
 
 
-
+## %######################################################%##
+#                                                          #
+####                    SpeciesLink                      ####
+#                                                          #
+## %######################################################%##
 
 # Baixar diretamente do SpeciesLink (https://specieslink.net/)
+occ_list <- list()
+for (i in seq_along(spp)) {
+  occ_list[[i]] <- rspeciesLink(
+    species = spp[i],
+    basisOfRecord = NULL,
+    Scope = "plants",
+    save = FALSE,
+    Coordinates = "Yes",
+    CoordinatesQuality = "Good"
+  ) %>%
+    tibble()
+  names(occ_list)[i] <- spp[i]
+}
+#IGNORE THIS MESSAGE: Output is empty. Check your request.
+
+occ_list <- dplyr::bind_rows(occ_list, .id = "search_name")
+occ_list$datecollected <- paste(occ_list$year, occ_list$month, occ_list$day, sep = "-")
+occ_list$datecollected <- lubridate::ymd(occ_list$datecollected)
+
+readr::write_csv(occ_list, file.path(dirs, "SPECIESLINK.csv")) # save as csv
 
 
+## %######################################################%##
+#                                                          #
+####                    GBIF                            ####
+#                                                          #
+## %######################################################%##
 # Global Biodiversity Information Facility (https://www.gbif.org/)
+occ_list <- list() # an empty list
+for (i in seq_along(spp)) {
+  message(paste(i, Sys.time()))
+  occ_list[[i]] <-
+    rgbif::occ_data(
+      scientificName = spp[i],
+      hasCoordinate = TRUE,
+      hasGeospatialIssue = FALSE, # Without spatial issues
+      limit = 20000
+    )
+  names(occ_list)[i] <- spp[i]
+}
+class(occ_list[[1]]) # it's a gbif_data not a data.frame or tibble
+
+# Let's extract the tibble (a kind of data.frame) object for each species
+for (i in seq_along(occ_list)) {
+  occ_list[[i]] <- occ_list[[i]]$data
+}
 
 
+# let's merge these tibbles
+occ_list <- dplyr::bind_rows(occ_list, .id = "search_name")
+names(occ_list)
+
+occ_list <- occ_list %>% dplyr::select(-networkKeys)
+
+readr::write_csv(occ_list, file.path(dirs, "GBIF.csv")) # save as csv
+
+
+## %######################################################%##
+#                                                          #
+####                    IDGBIO                          ####
+#                                                          #
+## %######################################################%##
 # iDigBio (https://www.idigbio.org/)
+occ_list <- list()
+for (i in seq_along(spp)) {
+  message(paste(i, Sys.time()))
+  occ_list[[i]] <-
+    ridigbio::idig_search_records(rq = list(scientificname = spp[i]), limit = 100000) %>%
+      tibble()
+  names(occ_list)[i] <- spp[i]
+}
+
+occ_list <- occ_list[sapply(occ_list, function(x) nrow((x))) > 0] # this line is only for removing
+occ_list <- dplyr::bind_rows(occ_list, .id = "search_name")
 
 
+# Extract year from date collected
+occ_list$datecollected <- lubridate::ymd(occ_list$datecollected) # 57 failed to parse
+occ_list$year <- lubridate::year(occ_list$datecollected)
+
+readr::write_csv(occ_list, file.path(dirs, "IDIGBIO.csv")) # save as csv
+
+
+## %######################################################%##
+#                                                          #
+####                    iNATURALIST                     ####
+#                                                          #
+## %######################################################%##
 # iNaturalist (https://www.inaturalist.org/)
 
+occ_list <- list()
+for (i in seq_along(spp)) {
+  message(paste(i, Sys.time()))
+  try(res <- rinat::get_inat_obs(query = spp[i], quality = "research", geo = TRUE, maxresults = 10000))
+  try(res <- res %>% dplyr::filter(iconic_taxon_name == "Plantae", captive_cultivated == "false") %>% as_tibble())
+  try(occ_list[[i]] <- res)
+  try(rm(res))
+  names(occ_list)[i] <- spp[i]
+} # Don't worry about these try() just catch the errors and continue the loop
 
-# SibBr (https://www.sibbr.gov.br/).
+occ_list <- occ_list[names(unlist(sapply(occ_list, nrow)))] # Checking rinat ouptut for valid species output and filtering out NA
+
+occ_list <- dplyr::bind_rows(occ_list, .id = "search_name") %>% as_tibble()
+
+# Extract year from date collected
+occ_list$year <- lubridate::year(occ_list$datetime)
+readr::write_csv(occ_list, file.path(dirs, "INATURALIST.csv")) # save as csv
 
 
 # Salvar em formato csv
+## %######################################################%##
+## %######################################################%##
+## %######################################################%##
+## %######################################################%##
+## %######################################################%##
+#                                                          #
+####               Using bdc package for                ####
+####   integrating and cleaning occurrences datasets    ####
+#                                                          #
+## %######################################################%##
+## %######################################################%##
+## %######################################################%##
+## %######################################################%##
+## %######################################################%##
+
+
+
+
+
+##%######################################################%##
+#                                                          #
+####        bdc: Integrating different databases         ####
+#                                                          #
+##%######################################################%##
+dirs <- file.path(getwd(), "raw_data")
+dirs
+
+### First copy and paste the "DatabaseInfo.txt" our "configuration table" in the raw_data folder ###
+
+# Read the configuration table
+metadata <- readr::read_tsv(file.path(dirs, "DatabaseInfo.txt"), show_col_types = FALSE)
+
+# Let's list different database
+list_db <- list.files(dirs, pattern = ".csv", full.names = TRUE)
+nms <- list_db %>%
+  basename() %>%
+  gsub(".csv", "", .)
+names(list_db) <- nms
+list_db <- dplyr::tibble(datasetName = names(list_db), fileName = list_db)
+list_db
+
+
+
+# Correct the fileName (path file)
+metadata$fileName <- NULL
+
+# Merge databases
+metadata <- dplyr::left_join(list_db, metadata, by = "datasetName")
+
+# Standarize datasets
+database <-
+  bdc::bdc_standardize_datasets(
+    metadata = metadata,
+    format = "csv",
+    overwrite = TRUE,
+    save_database = TRUE
+  )
+
+
+# bdc_standardize_datasets created a system of folders in "Documents"
+# So, the merged database in is:
+# C:/Users/santi/Documents/Output/Intermediate/00_merged_database.csv
+# use this function: readr::write_csv(database, FILE PATH)
+
+
+
+
+##%######################################################%##
+#                                                          #
+####     Some data exploration to convince you why      ####
+####     it is important to clean up your database      ####
+#                                                          #
+##%######################################################%##
+
+# Let's read this and explore this database
+dirs <- file.path(getwd(), "raw_data")
+dir2 <- file.path(dirname(dirs), "Output/Intermediate/00_merged_database.csv")
+database <- readr::read_csv(dir2, show_col_types = FALSE)
+database
+
+# Names used for searching occurrences
+database$searchName %>% unique()
+
+# Names returned by different databases
+database$scientificName %>%
+  unique() %>%
+  sort()
+
+# Names of the countries
+database$country %>%
+  unique() %>%
+  sort()
+
+# Years
+ggplot(database, aes(year)) + geom_histogram() + theme_bw()
+table(!is.na(database$year)) # many data not have year and other are very old
+
+# Patterns of occurrences
+w <- sf::st_as_sf(map("world", plot = FALSE, fill = TRUE))
+ggplot(w) +
+  geom_sf() +
+  geom_hex(data = database, aes(decimalLongitude, decimalLatitude), binwidth = c(4, 4)) +
+  scale_fill_viridis_c() +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+
+# Keep 00_merged_database.csv file because it will be use in the 02_occ_data_cleaning.R
