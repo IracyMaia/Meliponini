d4a4b8f04b381324cf731f7fce676b63f48b607f
Iracy Maiany <iracymaiany@hotmail.com>
2023-05-09 12:28:27 -0300

    Edition text

---
+++ Databases download and integration.R
@@ -0,0 +1,38 @@
+# pacots para usar
+devtools::install_github("liibre/Rocc")
+library(Rocc)
+require(dplyr) # Manipulate data
+require(readr) # Read and write data
+require(ridigbio) # Download data from iDigBio
+require(rgbif) # Download data from GBIF
+require(rinat) # Download data from inaturaList
+require(Rocc) # Download data from speciesLink
+
+
+# Despite bdc is available on CRAN for this class we advice to install the development version
+# available on GitHub (installed above)
+require(bdc) # Biodiversity data cleaning https://brunobrr.github.io/bdc/index.html
+require(ggplot2) # Plot data
+require(sf) # For handling spatial data
+require(maps) # A spatial database of country boundaries
+
+
+
+
+
+# Baixar diretamente do SpeciesLink (https://specieslink.net/)
+
+
+# Global Biodiversity Information Facility (https://www.gbif.org/)
+
+
+# iDigBio (https://www.idigbio.org/)
+
+
+# iNaturalist (https://www.inaturalist.org/)
+
+
+# SibBr (https://www.sibbr.gov.br/).
+
+
+# Salvar em formato csv
--- Instalacao e configuracao.R
+++ Instalacao e configuracao.R
@@ -1,4 +1,5 @@
 #Projeto Meliponini
+# by: Santiago J.E. Velazco <sjevelazco@gmail.com>
 
 #Pacotes para baixar
 # Codes for installing package from CRAN:
@@ -29,43 +30,3 @@ devtools::install_github("brunobrr/bdc")
 devtools::install_github("liibre/rocc")
 devtools::install_github("sjevelazco/flexsdm")
 devtools::install_github("andrefaa/ENMTML")
-
-# pacots para usar
-devtools::install_github("liibre/Rocc")
-library(Rocc)
-require(dplyr) # Manipulate data
-require(readr) # Read and write data
-require(ridigbio) # Download data from iDigBio
-require(rgbif) # Download data from GBIF
-require(rinat) # Download data from inaturaList
-require(Rocc) # Download data from speciesLink
-
-
-# Despite bdc is available on CRAN for this class we advice to install the development version
-# available on GitHub (installed above)
-require(bdc) # Biodiversity data cleaning https://brunobrr.github.io/bdc/index.html
-require(ggplot2) # Plot data
-require(sf) # For handling spatial data
-require(maps) # A spatial database of country boundaries
-
-
-
-
-
-# Baixar diretamente do SpeciesLink (https://specieslink.net/)
-
-
-# Global Biodiversity Information Facility (https://www.gbif.org/)
-
-
-# iDigBio (https://www.idigbio.org/)
-
-
-# iNaturalist (https://www.inaturalist.org/)
-
-
-# SibBr (https://www.sibbr.gov.br/).
-
-
-# Salvar em formato csv
-
